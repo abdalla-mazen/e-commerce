63909ec6b34e49e801b49c6a9bfb7217080e0303
abdalla-mazen <armn404040@gmail.com>
2025-02-28 20:35:17 +0200

    modifiyTwo

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -8,7 +8,7 @@ import { useContext } from 'react';
 import { UserContext } from '../../Context/UserContext';
 import { CartContext } from '../../Context/CartContext';
 
-
+import { initFlowbite } from 'flowbite'
 
 export default function Nav() {
  
@@ -39,7 +39,7 @@ let navigate=useNavigate()
 
   
   useEffect(() => {
- 
+    initFlowbite();
     if (isDarkMode) {
       document.documentElement.classList.add('dark');
       localStorage.setItem('dark-mode', 'true');
@@ -68,7 +68,7 @@ let navigate=useNavigate()
             </Link>
 
           </div>
-          <div className="left flex  items-center justify-center   gap-2">
+          <div  className="left flex  items-center justify-center   gap-2">
             <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
               <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
